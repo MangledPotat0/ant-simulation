8959adf0a0ed5f22d3fb6aadc17b91118f9a896a
mangledp0tat0 <CephalopodMaster@protonmail.com>
2021-01-29 15:02:20 -0500

    Outlined rough structure

--- python/defrag.py
+++ python/defrag.py
@@ -1,5 +1,6 @@
 ################################################################################
 #                                                                              #
+#                                                                              #
 ################################################################################
 
 import argparse
@@ -8,8 +9,44 @@ import numpy as np
 import os
 import sys
 
-if __name__ == '__main__':
 
+def setpath(fname):
+    currentdir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(currentdir)
+    filepath = '../../data/trajectories/'
+
+    return fiepath
+
+
+def loaddata(filepath, fname):
+    
+    ff = h5py.File('{}{}.h5py'.format(filepath, fname), 'a')
+    data = ff['trajectories']
+
+    return data
+
+
+def defragment():
+
+    return
+
+
+def sift():
+
+    return
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--file', help = 'Data file name without extension')
+    args = vars(ap.parse_args())
+
+    fname = args['file']
+    filepath = setpath(fname)
+    data = loaddata(filepath, fname)
+
+    defragment()
+    sift()
 
     sys.exit(0)
 
