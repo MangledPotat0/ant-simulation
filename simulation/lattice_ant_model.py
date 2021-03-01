99ae86f9185569e849c2f7cada5093e0cff7dec6
mangledp0tat0 <CephalopodMaster@protonmail.com>
2021-03-01 14:58:44 -0500

    Continuation on lattice model sim

--- simulation/lattice_ant_model.py
+++ simulation/lattice_ant_model.py
@@ -19,9 +19,11 @@
 
 import h5py
 import json
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import random
 import sys
 import time
 
@@ -36,6 +38,7 @@ class lattice_ant_model():
             self.nspecies_ = param['n_species']
             self.mcount_ = params['m_count']
             self.interaction_ = params['interaction_table']
+            self.lengthscale_ = params['length_scale']
 
             # These parameters are for the metropolis algorithm used for MCMC.
             self.threshold_ = params['threshold']
@@ -82,6 +85,9 @@ class lattice_ant_model():
     def interaction(self):
         return self.interaction_
 
+    def lengthscale(self):
+        return self.lengthscale_
+
     def threshold(self):
         return self.threshold_
 
@@ -89,19 +95,40 @@ class lattice_ant_model():
         return self.energyscale_
 
     def temperature(self):
-        return self.temperature
+        return self.temperature_
 
     
     def compute_damping(self):
+        damp_length = self.latticesize ** 2
+        damparray = np.zeros((damp_length, damp_length))
+        for i in range(damp_length):
+            for f in range(damp_length):
+                xdistance = abs(f % self.latticesize - i % self.latticesize) 
+                ydistance = abs(math.floor(f / self.latticesize)
+                                - math.floor(i / self.latticesize))
+                distance = math.sqrt(xdistance ** 2 + ydistance ** 2)
+                damparray[i,f] = math.exp(-lengthscale * distance)
 
-        return
+        return damparray
 
 
     def compute_energy(self):
-        energy = np.sum(np.dot(self.interaction, self.damping))
+        energy = np.sum(np.dot(self.interaction, self.damparray))
+        return energy
 
+
+    def compute_dE(self, start, end):
+        dE = np.sum(np.dot((damping[:, end] - damping[x,start]), vecn[:,i])
+                    + 2jj[l,l] * (1 - damping[end, start]))
+        return dE
+
+    def run(self, tt):
+        ct = 0
+        while ct < tt:
+            ct += 1
+            anttype = random.randint((1,n))
+            flattened = np.flatten(self.lattice)
         return
-
 
 if __name__ == '__main__':
     print('Process completed successfully. Exiting.')
