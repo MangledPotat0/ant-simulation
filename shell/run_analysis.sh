32f475abe96df0b1781df1d2c9a5afc38cc77d07
maelstrom <cephalopodmaster@protonmail.com>
2022-01-05 14:14:26 -0500

    Fixed the inference command

--- shell/run_analysis.sh
+++ shell/run_analysis.sh
@@ -14,7 +14,7 @@ sleapdir=${wd}/data/sleap
 cd $wd
 
 # convert raw h264 video into mp4
-ffmpeg -i ${rawdir}/${fname}.h264 ${vidd}/${fname}.mp4 -y
+#ffmpeg -i ${rawdir}/${fname}.h264 ${vidd}/${fname}.mp4 -y
 
 # Crop video to preset dimensions
 # python ${pydir}/cropper.py -v ${vidd}/${fname} -c 100 100 100 100
@@ -23,15 +23,33 @@ ffmpeg -i ${rawdir}/${fname}.h264 ${vidd}/${fname}.mp4 -y
 fname=${fname}cropped
 
 # Run SLEAP detection and initial tracking
+#sleap-track ${vidd}/${fname}.mp4 \
+#	--video.input_format channels_last \
+#	--frames 0-599 \
+#	-m ${sleapdir}/current/centered/training_config.json \
+#	-m ${sleapdir}/current/centroid/training_config.json \
+#	--tracking.tracker simple \
+#	--verbosity none \
+#	--no-empty-frames \
+#	-o ${sleapdir}/${fname}/${fname}.slp
+
+#--labels /app/antproject/data/sleap/tags/n60tagging/n60.slp --only-suggested-frames
+
 sleap-track ${vidd}/${fname}.mp4 \
-	--video.input_format channels_last \
-	-frames 0-599 \
-	-m ${sleapdir}/current/centered/training_config.json \
-	-m ${sleapdir}/current/centroid/training_config.json \
-	--tracking.tracker simple \
-	--verbosity json \
-	--no-empty-frames \
-	-o ${sleapdir}/${fname}/${fname}.slp
+	--frames 0-599 \
+	-m /app/antproject/data/sleap/current/centroid/training_config.json \
+       	-m /app/antproject/data/sleap/current/centered/training_config.json \
+       	--tracking.tracker simple \
+	--tracking.pre_cull_to_target 0 \
+	--tracking.pre_cull_iou_threshold 0.8 \
+	--tracking.similarity iou \
+	--tracking.match hungarian \
+	--tracking.track_window 5 \
+	--tracking.post_connect_single_breaks 0 \
+	-o ${sleapdir}/tags/${fname}_600frames.slp \
+	--verbosity none \
+	--no-empty-frames
+
 
 # Reformat SLEAP output
 
